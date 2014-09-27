34de10a0d41c0b06925394415762f1c68fa0f122
ninthday <jeffy@ninthday.info>
2014-09-27 18:12:09 +0800

    1. 在 query_manager 中新增 getSearchBin 函數，只有回傳 Search Bin 資料 2. 修改 index 呈現欄位

--- capture/query_manager.php
+++ capture/query_manager.php
@@ -675,6 +675,46 @@ function getBins() {
     return $querybins;
 }
 
+/**
+ * 取得 Search Bins 的資料列表
+ *
+ * @return array Search Bin Content
+ * @author ninthday <jeffy@ninthday.info>
+ * @since 2014-09-27
+ */
+function getSearchBins(){
+    $dbh = pdo_connect();
+    $sql = 'SELECT `tcat_query_bins`.`id`, `tcat_query_bins`.`querybin`, `description`, `origin_phrase`, `createtime`, `updatetime`
+            FROM `tcat_query_bins`
+            INNER JOIN `tcat_search_timetable` ON `tcat_search_timetable`.`querybin_id` = `tcat_query_bins`.`id`';
+    $rec = $dbh->prepare($sql);
+    $rec->execute();
+    $rs_sbin = $rec->fetchAll();
+    $querybins = array();
+    foreach ($rs_sbin as $data) {
+        if (!isset($querybins[$data['id']])) {
+            $bin = new stdClass();
+            $bin->id = $data['id'];
+            $bin->name = $data['querybin'];
+            $bin->phrases = $data['origin_phrase'];
+            $bin->createtime = $data['createtime'];
+            $bin->updatetime = $data['updatetime'];
+            $bin->description = $data['description'];
+
+            $sql = "SELECT count(id) AS count FROM " . $data['querybin'] . "_tweets";
+            $res = $dbh->prepare($sql);
+            if ($res->execute() && $res->rowCount()) {
+                $result = $res->fetch();
+                $bin->nrOfTweets = $result['count'];
+            }
+        }
+        $querybins[$data['id']] = $bin;
+    }
+
+    $dbh = false;
+    return $querybins;
+}
+
 function getLastRateLimitHit() {
     $dbh = pdo_connect();
     $rec = $dbh->prepare("SELECT end FROM tcat_error_ratelimit ORDER BY end DESC LIMIT 1");
--- capture/search/index.php
+++ capture/search/index.php
@@ -18,153 +18,160 @@ create_error_logs();
 
 $captureroles = unserialize(CAPTUREROLES);
 
-$querybins = getBins();
+$querybins = getSearchBins();
 ?>
 
 <html>
-    <head>
-        <title>FFtcat - Flood Fire Twitter Capturing and Analysis Toolset</title>
-        <meta charset='<?php echo mb_internal_encoding(); ?>'>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-        <style type="text/css">
-            body,html { font-family: "Comic Sans MS", cursive, sans-serif; padding: 2px; font-size:14px; }
-            table { overflow:hidden; border-radius: 5px; font-size:14px; }
-            .form-control{ width: 400px; margin: 5px; }
-            th { background-color: #ccc; padding:8px; }
-            td { background-color: #eee; padding:8px; }
-            .row { padding-left: 30px; padding-right: 30px; }
-            .keywords { width:600px; }
-        </style>
-    </head>
-    <body>
-        <h1>Flood Fire - Twitter Capturing and Analysis Toolset</h1>
-            <form onsubmit="sendNewForm(); return false;">
-                <div class="form-group">
-                    <label for="newbin_name" class="col-sm-2 control-label">Query Bin</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="newbin_name" placeholder="Bin name">
-                        <li>之後不可修改。</li>
-                        <br>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPhrase" class="col-sm-2 control-label">Phrase to search</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputPhrase" placeholder="Phrases">
-                        <li>以OR區別關鍵字，例如:台灣 OR 中華民國 OR Taiwan OR "Republic of China"</li>
-                        <br>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPhrase" class="col-sm-2 control-label">Description</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputDescription" placeholder="Description">
-                        <br>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-default">Create query bin</button>
-                        <br>
-                        <br>
-                    </div>
-                </div>
-            </form>
-    <h2>Query manager</h2>
-        <div class="row">
-            <table align="center" class="table table-hover">
-            <thead>
-                <tr>
-                    <th>Querybin</th>
-                    <th>Description</th>
-                    <th class="keywords">Queries</th>
-                    <th>No. tweets</th>
-                    <th>Periods in which the query bin was active</th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php
-            foreach ($querybins as $bin) {
-                $phraseList = array();
-                $phrasePeriodsList = array();
-                $activePhraseList = array();
+<head>
+    <title>FFtcat - Flood Fire Twitter Capturing and Analysis Toolset</title>
+    <meta charset='<?php echo mb_internal_encoding(); ?>'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <style type="text/css">
+        body, html {
+            font-family: "Comic Sans MS", cursive, sans-serif;
+            padding: 2px;
+            font-size: 14px;
+        }
 
-                if ($bin->type == 'search') {
-                    foreach ($bin->phrases as $phrase) {
-                        $phrasePeriodList[$phrase->id] = array_unique($phrase->periods);
-                        $phraseList[$phrase->id] = $phrase->phrase;
-                        if ($phrase->active) {
-                            $activePhraselist[$phrase->id] = $phrase->phrase;
-                        }
-                    }
-                }
-                $bin->periods = array_unique($bin->periods);
-                sort($bin->periods);
-                asort($phraseList);
+        table {
+            overflow: hidden;
+            border-radius: 5px;
+            font-size: 14px;
+        }
 
-                $action = ($bin->active == 0) ? "start" : "stop";
-                if ($bin->type == 'search')
-                {
-                    echo '<tr>';
-                    echo '<td valign="top">' . $bin->name . '</td>';
-                    echo '<td valign="top">' . $bin->active . '</td>';
-                    echo '<td class="keywords" valign="top">';
-                    echo '<table width="100%">';
-                    foreach ($phraseList as $phrase_id => $phrase)
-                    {
-                        echo "<td valign='top'>$phrase</td>";
-                    }
-                    echo '</table>';
-                    echo '</td>';
-                    echo '<td valign="top" align="right"> ' . number_format($bin->nrOfTweets, 0, ",", ".") . '</td>';
-                    echo '<td valign="top" align="right"> ' . implode("<br />", $bin->periods) . '</td>';
-                    echo '</tr>';
-                }
-            }
-            ?>
-                </tbody>
-            </table>
+        .form-control {
+            width: 400px;
+            margin: 5px;
+        }
+
+        th {
+            background-color: #ccc;
+            padding: 8px;
+        }
+
+        td {
+            background-color: #eee;
+            padding: 8px;
+        }
+
+        .row {
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+
+        .keywords {
+            width: 600px;
+        }
+    </style>
+</head>
+<body>
+<h1>Flood Fire - Twitter Capturing and Analysis Toolset</h1>
+
+<form onsubmit="sendNewForm(); return false;">
+    <div class="form-group">
+        <label for="newbin_name" class="col-sm-2 control-label">Query Bin</label>
+
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="newbin_name" placeholder="Bin name">
+            <li>之後不可修改。</li>
+            <br>
         </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPhrase" class="col-sm-2 control-label">Phrase to search</label>
 
-    <script type='text/javascript' src='../../analysis/scripts/jquery-1.7.1.min.js'></script>
-    <script type="text/javascript">
-        function sendNewForm() {
-            var _bin = $("#newbin_name").val();
-            var _description = $("#inputDescription").val();
-            if(!validateBin(_bin))
-                return false;
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputPhrase" placeholder="Phrases">
+            <li>以OR區別關鍵字，例如:台灣 OR 中華民國 OR Taiwan OR "Republic of China"</li>
+            <br>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPhrase" class="col-sm-2 control-label">Description</label>
 
-            var _phrases = $("#inputPhrase").val();
-            var _check = window.confirm("You are about to create a new search query bin. Are you sure?");
-            if(_check == true) {
-                var _params = {action:"newbin",type:"search",newbin_phrases:_phrases,newbin_name:_bin,description:_description,active:$("#make_active").val()};
-                $.ajax({
-                    dataType: "json",
-                    url: "../query_manager.php",
-                    type: 'POST',
-                    data: _params
-                }).done(function(_data) {
-                    alert(_data["msg"]);
-                    location.reload();
-                });
-            }
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputDescription" placeholder="Description">
+            <br>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Create query bin</button>
+            <br>
+            <br>
+        </div>
+    </div>
+</form>
+<h2>Query manager</h2>
+
+<div class="row">
+    <table align="center" class="table table-hover">
+        <thead>
+        <tr>
+            <th>Querybin</th>
+            <th class="keywords">Phrases</th>
+            <th>No. tweets</th>
+            <th>Created Time</th>
+            <th>Last Update</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($querybins as $bin) {
+            echo '<tr>';
+            echo '<td>' . $bin->name . '</td>';
+            echo '<td>' . implode(', ', explode("OR", $bin->phrases)) . '</td>';
+            echo '<td align="center"> ' . number_format($bin->nrOfTweets) . '</td>';
+            echo '<td align="center"> ' . $bin->createtime . '</td>';
+            echo '<td align="center"> ' . $bin->updatetime . '</td>';
+            echo '<td> ' . $bin->description . '</td>';
+            echo '</tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+
+<script type='text/javascript' src='../../analysis/scripts/jquery-1.7.1.min.js'></script>
+<script type="text/javascript">
+    function sendNewForm() {
+        var _bin = $("#newbin_name").val();
+        var _description = $("#inputDescription").val();
+        if (!validateBin(_bin))
+            return false;
+
+        var _phrases = $("#inputPhrase").val();
+        var _check = window.confirm("You are about to create a new search query bin. Are you sure?");
+        if (_check == true) {
+            var _params = {action: "newbin", type: "search", newbin_phrases: _phrases, newbin_name: _bin, description: _description, active: $("#make_active").val()};
+            $.ajax({
+                dataType: "json",
+                url: "../query_manager.php",
+                type: 'POST',
+                data: _params
+            }).done(function (_data) {
+                alert(_data["msg"]);
+                location.reload();
+            });
+        }
+        return false;
+    }
+
+    function validateBin(binname) {
+        if (binname == null || binname.trim() == "") {
+            alert("You cannot use an empty bin name");
             return false;
         }
-
-        function validateBin(binname) {
-            if(binname == null || binname.trim()=="") {
-                alert("You cannot use an empty bin name");
-                return false;
-            }
-            var reg = /^[a-zA-Z0-9_]+$/;
-            if(!reg.test(binname.trim())) {
-                alert("bin names can only consist of alpha-numeric characters and underscores")
-                return false;
-            }
-            return true;
+        var reg = /^[a-zA-Z0-9_]+$/;
+        if (!reg.test(binname.trim())) {
+            alert("bin names can only consist of alpha-numeric characters and underscores")
+            return false;
         }
+        return true;
+    }
 
-    </script>
-    </body>
+</script>
+</body>
 </html>
